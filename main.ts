45a34d9c9a56906616635ef9551862359a9f35d7
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-18 12:26:16 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-game.splash("Going in Stealth or Loud? A for stealth, B for loud")
+let Heister2: Sprite = null
+let Heister1: Sprite = null
+game.splash("Press \"A\" for stealth, \"B\" for loud")
 if (controller.A.isPressed()) {
-	
+    Heister1 = sprites.create(assets.image`Austin`, SpriteKind.Player)
+    Heister2 = sprites.create(assets.image`Luxembourg`, SpriteKind.Player)
+    tiles.setCurrentTilemap(tilemap`Bank_Entrance_Outside`)
 } else {
 	
 }
+controller.moveSprite(Heister1)
+controller.player2.moveSprite(Heister2)
+scene.cameraFollowSprite(Heister1)
