4d2ca38111817ff3d5705ac5d5eb3d74cff90d6c
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-18 15:28:48 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,9 @@
-controller.player2.onEvent(ControllerEvent.Connected, function () {
-    Heister2 = sprites.create(assets.image`Luxembourg`, SpriteKind.Player)
-    controller.moveSprite(Heister2)
-    splitScreen.setSplitScreenEnabled(true)
-    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Heister1)
-    splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.VerticalLeftHalf)
-    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Heister2)
-    splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.VerticalRightHalf)
-})
-let Heister2: Sprite = null
-let Heister1: Sprite = null
 game.splash("Press \"A\" for stealth, \"B\" for loud")
 if (controller.A.isPressed()) {
     tiles.setCurrentTilemap(tilemap`level1`)
 } else if (controller.B.isPressed()) {
     tiles.setCurrentTilemap(tilemap`level2`)
 }
-Heister1 = sprites.create(assets.image`Austin`, SpriteKind.Player)
-Heister1 = mp.playerSelector(mp.PlayerNumber.One)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
+let Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
