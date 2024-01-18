4d84a6b6c493a782a8f1883efcb45c0d922b892a
Adriel De Los Reyes <156137490+Taxicol@users.noreply.github.com>
2024-01-18 15:57:55 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -13,8 +13,6 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile7 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile8 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
     export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile10 = image.ofBuffer(hex``);
@@ -28,6 +26,50 @@ namespace myTiles {
     export const tile12 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile25 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile26 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile27 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile28 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile29 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile30 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile32 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile31 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile33 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile34 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -105,8 +147,6 @@ namespace myTiles {
             case "tile6":return tile6;
             case "Bank5":
             case "tile7":return tile7;
-            case "Bank6":
-            case "tile8":return tile8;
             case "Bank7":
             case "tile9":return tile9;
             case "Bank8":
@@ -120,6 +160,50 @@ namespace myTiles {
             case "Bank10":
             case "tile12":return tile12;
             case "tile13":return tile13;
+            case "Bank17":
+            case "tile20":return tile20;
+            case "Bank6":
+            case "tile8":return tile8;
+            case "Bank11":
+            case "tile14":return tile14;
+            case "Bank12":
+            case "tile15":return tile15;
+            case "Bank13":
+            case "tile16":return tile16;
+            case "Bank14":
+            case "tile17":return tile17;
+            case "Bank15":
+            case "tile18":return tile18;
+            case "Bank16":
+            case "tile19":return tile19;
+            case "Bank18":
+            case "tile21":return tile21;
+            case "Bank19":
+            case "tile22":return tile22;
+            case "Bank20":
+            case "tile23":return tile23;
+            case "Bank21":
+            case "tile24":return tile24;
+            case "Bank22":
+            case "tile25":return tile25;
+            case "Bank23":
+            case "tile26":return tile26;
+            case "Bank24":
+            case "tile27":return tile27;
+            case "Bank25":
+            case "tile28":return tile28;
+            case "Bank26":
+            case "tile29":return tile29;
+            case "Bank27":
+            case "tile30":return tile30;
+            case "Bank29":
+            case "tile32":return tile32;
+            case "Bank28":
+            case "tile31":return tile31;
+            case "Bank30":
+            case "tile33":return tile33;
+            case "Bank31":
+            case "tile34":return tile34;
         }
         return null;
     })
