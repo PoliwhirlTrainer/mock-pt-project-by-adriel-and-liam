ec882b5864e94a10dffb5acabe604b9e500ac9cc
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-18 15:57:07 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+namespace SpriteKind {
+    export const Loot_Pickup_type = SpriteKind.create()
+}
+function Pickuploot (LootType: string, LootValue: number, OnLoot: boolean) {
+    Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type)]
+}
+let Loot: Sprite[] = []
 game.splash("Press \"A\" for stealth, \"B\" for loud")
 if (controller.A.isPressed()) {
     tiles.setCurrentTilemap(tilemap`level1`)
