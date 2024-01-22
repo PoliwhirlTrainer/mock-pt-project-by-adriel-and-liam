4a8e3bce58559f223a09ea2d735b72626eb3b85b
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-22 11:05:56 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,26 +4,31 @@ namespace SpriteKind {
 function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
 }
-let PLayer2Health: StatusBarSprite = null
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Luxembourg`, SpriteKind.Player))
+    Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
+    mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+    PLayer2Health = statusbars.create(20, 4, StatusBarKind.Health)
+    PLayer2Health.setColor(9, 2, 3)
+    PLayer2Health.attachToSprite(Heister2)
+    Heister2.setStayInScreen(true)
+    splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Heister2)
+    splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.VerticalRightHalf)
+})
 let Player1Health: StatusBarSprite = null
+let PLayer2Health: StatusBarSprite = null
+let Heister2: Sprite = null
 let Loot: Sprite[] = []
 tiles.setCurrentTilemap(tilemap`level1`)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
 let Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Luxembourg`, SpriteKind.Player))
-let Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
-mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
+Heister1.setStayInScreen(true)
 splitScreen.setSplitScreenEnabled(true)
 splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Heister1)
 splitScreen.setCameraRegion(splitScreen.Camera.Camera1, splitScreen.CameraRegion.VerticalLeftHalf)
-splitScreen.cameraFollowSprite(splitScreen.Camera.Camera2, Heister2)
-splitScreen.setCameraRegion(splitScreen.Camera.Camera2, splitScreen.CameraRegion.VerticalRightHalf)
 forever(function () {
     Player1Health = statusbars.create(20, 4, StatusBarKind.Health)
     Player1Health.setColor(9, 2, 3)
     Player1Health.attachToSprite(Heister1)
-    PLayer2Health = statusbars.create(20, 4, StatusBarKind.Health)
-    PLayer2Health.setColor(9, 2, 3)
-    PLayer2Health.attachToSprite(Heister2)
 })
