43f4ee7247fa0989eaa169baae140f0cf99f08f1
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-22 16:11:36 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -5,17 +5,12 @@ function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
 }
 controller.player2.onEvent(ControllerEvent.Connected, function () {
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Luxembourg`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Medic`, SpriteKind.Player))
     Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
     tiles.placeOnTile(Heister2, tiles.getTileLocation(20, 36))
     mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
-    PLayer2Health = statusbars.create(20, 4, StatusBarKind.Health)
-    PLayer2Health.setColor(9, 2)
-    PLayer2Health.attachToSprite(Heister2)
-    Heister2.setStayInScreen(true)
 })
 let Player1Health: StatusBarSprite = null
-let PLayer2Health: StatusBarSprite = null
 let Heister2: Sprite = null
 let Loot: Sprite[] = []
 scene.setBackgroundImage(assets.image`sky`)
