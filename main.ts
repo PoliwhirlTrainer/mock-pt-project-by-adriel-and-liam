3b58c557f785455658de5a18275a024f03d6b1cb
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-22 16:04:13 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -4,24 +4,23 @@ namespace SpriteKind {
 function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
 }
+statusbars.onStatusReached(StatusBarKind.Health, statusbars.StatusComparison.LT, statusbars.ComparisonType.Percentage, 25, function (status) {
+    Heister1.sayText("MEDIC!!")
+})
 controller.player2.onEvent(ControllerEvent.Connected, function () {
-    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`MEDIC`, SpriteKind.Player))
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Luxembourg`, SpriteKind.Player))
     Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
     tiles.placeOnTile(Heister2, tiles.getTileLocation(20, 36))
     mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.Two))
-    PLayer2Health = statusbars.create(20, 4, StatusBarKind.Health)
-    PLayer2Health.setColor(9, 2)
-    PLayer2Health.attachToSprite(Heister2)
-    Heister2.setStayInScreen(true)
 })
 let Player1Health: StatusBarSprite = null
-let PLayer2Health: StatusBarSprite = null
 let Heister2: Sprite = null
 let Loot: Sprite[] = []
+let Heister1: Sprite = null
 scene.setBackgroundImage(assets.image`sky`)
 tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
-let Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
+Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 tiles.placeOnTile(Heister1, tiles.getTileLocation(20, 36))
 controller.moveSprite(Heister1)
