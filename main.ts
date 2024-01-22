7504d39e19c070ad805ae4718561817b9d35ca7f
Liam M <156138068+PoliwhirlTrainer@users.noreply.github.com>
2024-01-22 15:09:27 -0600

    Restore '1/22/2024, 2:59:04 PM Restore '1/22/2024, 1:02:51 PM Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts''

--- main.ts
+++ main.ts
@@ -5,15 +5,33 @@ function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
 }
 controller.player2.onEvent(ControllerEvent.Connected, function () {
-    let Heister2: Sprite = null
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Luxembourg`, SpriteKind.Player))
     controller.moveSprite(Heister2)
+    Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
+    PLayer2Health = statusbars.create(20, 4, StatusBarKind.Health)
+    PLayer2Health.setColor(9, 2)
+    PLayer2Health.attachToSprite(Heister2)
+    Heister2.setStayInScreen(true)
 })
 controller.player1.onEvent(ControllerEvent.Connected, function () {
-    let Heister1: Sprite = null
     controller.moveSprite(Heister1)
 })
+let Player1Health: StatusBarSprite = null
+let PLayer2Health: StatusBarSprite = null
 let Loot: Sprite[] = []
+let Heister2: Sprite = null
+let Heister1: Sprite = null
 tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
+mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
+Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
+mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
+tiles.placeOnTile(Heister1, tiles.getTileLocation(20, 36))
+Heister1.setStayInScreen(true)
+splitScreen.setSplitScreenEnabled(true)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Heister1)
+splitScreen.cameraFollowSprite(splitScreen.Camera.Camera1, Heister2)
 forever(function () {
-	
+    Player1Health = statusbars.create(20, 4, StatusBarKind.Health)
+    Player1Health.setColor(9, 2)
+    Player1Health.attachToSprite(Heister1)
 })
