d7b7acbaab312012298216de2e7823124b9db6be
Adriel De Los Reyes <156137490+Taxicol@users.noreply.github.com>
2024-01-24 10:08:47 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,26 @@ function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
-	
+    currentmap += 1
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
-	
+    currentmap += 1
 })
+function levels (maps: number) {
+    if (maps == 0) {
+        tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
+    } else if (maps == 1) {
+    	
+    } else if (maps == 2) {
+    	
+    } else if (maps == 3) {
+    	
+    } else if (maps == 4) {
+    	
+    } else if (maps == 5) {
+    	
+    }
+}
 controller.player2.onEvent(ControllerEvent.Connected, function () {
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Medic`, SpriteKind.Player))
     Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
@@ -20,7 +35,7 @@ let Player1Health: StatusBarSprite = null
 let Heister2: Sprite = null
 let Loot: Sprite[] = []
 scene.setBackgroundImage(assets.image`sky`)
-tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
+let currentmap = 0
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
 let Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
