f2b4b835b10b185d26b19d204d93b73c9e0ac456
Adriel De Los Reyes <156137490+Taxicol@users.noreply.github.com>
2024-01-26 09:23:43 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 namespace SpriteKind {
     export const Loot_Pickup_type = SpriteKind.create()
 }
+function Levels (Currentmap: number) {
+    if (Currentmap == 0) {
+        tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
+    } else if (Currentmap == 1) {
+        tiles.setCurrentTilemap(tilemap`bank_main_area`)
+    } else if (Currentmap == 2) {
+        tiles.setCurrentTilemap(tilemap`ATM`)
+    } else if (Currentmap == 3) {
+        tiles.setCurrentTilemap(tilemap`vault_entrance`)
+    } else if (Currentmap == 4) {
+        tiles.setCurrentTilemap(tilemap`vault_interior`)
+    } else if (Currentmap == 5) {
+        tiles.setCurrentTilemap(tilemap`level39`)
+    }
+}
 function initialmap (map: any[]) {
-    mySprite = tiles.getTilesByType(assets.tile`transparency16`)[0]
+    playerstartlocation = tiles.getTilesByType(assets.tile`myTile22`)[0]
+    tiles.placeOnTile(Heister1, playerstartlocation)
+    tiles.setTileAt(playerstartlocation, assets.tile`transparency16`)
 }
 function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
     Loot = [sprites.create(assets.image`Money_Bag`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Bag_of_GOLD`, SpriteKind.Loot_Pickup_type), sprites.create(assets.image`Jewlery_Box`, SpriteKind.Loot_Pickup_type)]
@@ -10,27 +27,24 @@ function SpawnLoot (LootType: string, LootValue: number, OnLoot: boolean) {
         tiles.placeOnRandomTile(Loot._pickRandom(), sprites.dungeon.chestOpen)
     }
 }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`endofmainarea`, function (sprite, location) {
+    currentmap += 2
+    if (nextmap()) {
+        Levels(currentmap)
+    }
+})
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
     currentmap += 1
+    if (nextmap()) {
+        Levels(currentmap)
+    }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
     currentmap += 1
-})
-function levels (maps: number) {
-    if (currentmap == 0) {
-        tiles.setCurrentTilemap(tilemap`Prototype_bank_entrance`)
-    } else if (currentmap == 1) {
-        tiles.setCurrentTilemap(tilemap`bank_main_area`)
-    } else if (currentmap == 2) {
-        tiles.setCurrentTilemap(tilemap`ATM`)
-    } else if (maps == 3) {
-        tiles.setCurrentTilemap(tilemap`vault_entrance`)
-    } else if (maps == 4) {
-        tiles.setCurrentTilemap(tilemap`vault_interior`)
-    } else if (maps == 5) {
-        tiles.setCurrentTilemap(tilemap`level39`)
+    if (nextmap()) {
+        Levels(currentmap)
     }
-}
+})
 controller.player2.onEvent(ControllerEvent.Connected, function () {
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`Medic`, SpriteKind.Player))
     Heister2 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two))
@@ -47,16 +61,17 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.floorDark2, function (spr
 let Player1Health: StatusBarSprite = null
 let Heister2: Sprite = null
 let Loot: Sprite[] = []
-let mySprite: tiles.Location = null
+let playerstartlocation: tiles.Location = null
+let Heister1: Sprite = null
 let currentmap = 0
 scene.setBackgroundImage(assets.image`sky`)
-levels(currentmap)
+currentmap = 0
+Levels(currentmap)
 mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`Austin`, SpriteKind.Player))
-let Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
+Heister1 = mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One))
 mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
 scene.cameraFollowSprite(Heister1)
 tiles.placeOnTile(Heister1, tiles.getTileLocation(20, 36))
-currentmap = 0
 forever(function () {
     Player1Health = statusbars.create(20, 4, StatusBarKind.Health)
     Player1Health.setColor(9, 2)
